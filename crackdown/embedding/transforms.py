d5d8639b11c8302ce819d8dc9c0a3a93ec279906
Ihor Omelchenko <counter3d@gmail.com>
2019-10-17 23:49:00 +0300

    Add CenterCrop and MaxPooling transformers

--- crackdown/embedding/transforms.py
+++ crackdown/embedding/transforms.py
@@ -4,6 +4,12 @@ from torchvision.transforms import *
 __all__ = [
     'ToNumpy',
     'Transpose',
+    'Scale',
+    'CenterCrop',
+    'MaxPooling',
+    'EMPTY',
+    'downscale',
+    'crop_center',
 ]
 
 
@@ -18,7 +24,7 @@ class ToNumpy:
 
 
 class Transpose:
-    def __init__(self, order):
+    def __init__(self, order=(2, 0, 1)):
         self.order = order
 
     def __call__(self, arr):
@@ -31,6 +37,35 @@ class Scale:
 
     def __call__(self, arr):
         return arr * self.factor
+
+
+class CenterCrop:
+    def __init__(self, size: int):
+        assert isinstance(size, int)
+
+        self.size = size
+
+    def __call__(self, arr):
+        shape = np.array(arr.shape[:2])
+        center = shape / 2
+        half_size = self.size / 2
+        left_up = (center - half_size).astype(np.int)
+        right_down = (center + half_size).astype(np.int)
+        return arr[tuple([slice(a, b) for a, b in zip(left_up, right_down)])]
+
+
+class MaxPooling:
+    def __init__(self, factor: int):
+        assert isinstance(factor, int)
+
+        self.factor = factor
+
+    def __call__(self, arr):
+        max_pool = self.factor
+        width, height, channels = arr.shape
+        arr = arr[:width // max_pool * max_pool, :height // max_pool * max_pool]
+        width, height, channels = arr.shape
+        return arr.reshape(width // max_pool, max_pool, height // max_pool, max_pool, -1).max(axis=(1, 3))
 
 
 EMPTY = transforms.Compose([
@@ -49,4 +84,12 @@ def downscale(shape: tuple, factor: float = 0.5):
         ToNumpy(),
         Transpose((2, 0, 1)),
         Scale(),
+    ])
+
+
+def crop_center(size: int):
+    return transforms.Compose([
+        CenterCrop(size),
+        Transpose((2, 0, 1)),
+        Scale()
     ])
