644a65361175682c7c9e66312db7e5b1a1dc1266
Ihor Omelchenko <counter3d@gmail.com>
2020-06-15 18:36:59 +0300

    Fix DQN reward and done matrixes shape bug

--- crackdown/algorithms/dqn/agent.py
+++ crackdown/algorithms/dqn/agent.py
@@ -1,13 +1,14 @@
 import copy
 from collections import OrderedDict
 import torch
+from torch import nn
 import gym
 
 from ...core.agent import Agent
 from ...core.report import Report
 from ... import transforms
 from ...embedding.image import ImageEmbedding
-from .quality_network import QualityNetwork
+from .quality_head import QualityHead
 from ...core.policy import make_action_head
 from ...utils import polyak_update
 
@@ -27,8 +28,8 @@ class DeepQualityNetworkAgent(Agent):
 
     def __init__(self,
                  env: gym.Env,
+                 embedding: nn.Module,
                  transform: transforms.Compose = None,
-                 embedding_capacity: int = 128,
                  discount_factor: float = 0.95,
                  target_update_factor: float = 0.99,
                  double_dqn: bool = True,
@@ -36,15 +37,14 @@ class DeepQualityNetworkAgent(Agent):
 
         super().__init__()
 
-        self.observation_transform = transform or transforms.EMPTY
-
         self.target_update_factor = target_update_factor
         self.double_dqn = double_dqn
         self.dueling_dqn = dueling_dqn
 
-        self.embedding = ImageEmbedding(env.observation_space.shape[-1], embedding_capacity, 5)
+        self.embedding = embedding
+        self.observation_transform = transform or transforms.EMPTY
         self.action_head = make_action_head(env.action_space)
-        self.quality_net = QualityNetwork(
+        self.quality_net = QualityHead(
             self.embedding.output_shape[-1],
             self.action_head.input_shape[-1],
             discount_factor,
@@ -54,10 +54,13 @@ class DeepQualityNetworkAgent(Agent):
         if double_dqn:
             self.target_quality_net = copy.deepcopy(self.quality_net)
 
-        if dueling_dqn:
-            self.value_net = QualityNetwork(self.embedding.output_shape[-1], 1, discount_factor)
-
         self.reset()
+
+    def on_transition(self, transition: dict) -> dict:
+        transition['state'] = self.observation_transform(transition['state'])
+        transition['next_state'] = self.observation_transform(transition['next_state'])
+
+        return transition
 
     def predict(self, state, deterministic: bool = False):
         with torch.no_grad():
@@ -72,6 +75,10 @@ class DeepQualityNetworkAgent(Agent):
     def update(self, batch: OrderedDict) -> dict:
         state, action, next_state, reward, done = batch.values()
 
+        # TODO: replace by "data preparing function"
+        done = done.float().unsqueeze(-1)
+        reward = reward.unsqueeze(-1)
+
         embedding = self.embedding.forward(state)
         next_embedding = self.embedding.forward(next_state)
 
@@ -81,9 +88,10 @@ class DeepQualityNetworkAgent(Agent):
             else:
                 next_quality = None
 
-        loss, advantage = self.quality_net.update(embedding, action, reward, next_embedding, next_quality)
+        loss, advantage = self.quality_net.update(embedding, action, reward, next_embedding, next_quality, done)
 
-        polyak_update(self.target_quality_net, self.quality_net, self.target_update_factor)
+        if self.double_dqn and self.target_update_factor > 0:
+            polyak_update(self.target_quality_net, self.quality_net, self.target_update_factor)
 
         return {
             'loss': loss,
--- crackdown/algorithms/dqn/quality_head.py
+++ crackdown/algorithms/dqn/quality_head.py
@@ -1,9 +1,10 @@
+import warnings
 from typing import Union
 import torch
 from torch import nn
 
 __all__ = [
-    'QualityNetwork',
+    'QualityHead',
 ]
 
 
@@ -16,43 +17,35 @@ def get_module_output_dim(model: nn.Module) -> int:
     return linear_layers[-1].out_features
 
 
-class QualityNetwork(nn.Module):
+class QualityHead(nn.Module):
     def __init__(self,
-                 embedding_dim: int = 16,
+                 input_dim: int = 16,
                  output_dim: int = 0,
                  discount_factor: float = 0.95,
                  dueling_dqn: bool = False):
 
         super().__init__()
 
-        self.embedding_dim = embedding_dim
+        self.input_dim = input_dim
         self.output_dim = output_dim
         self.discount_factor = discount_factor
         self.dueling_dqn = dueling_dqn
 
-        self.features = self.create_feature_extractor()
-        features_dim = get_module_output_dim(self.features)
-        self.q_net = nn.Linear(features_dim, output_dim, bias=True)
+        self.action_quality_net = nn.Linear(input_dim, output_dim, bias=True)
 
         if dueling_dqn:
-            self.dueling_q_net = nn.Linear(features_dim, 1, bias=True)
+            self.value_net = nn.Linear(input_dim, 1, bias=True)
 
         self.criterion = nn.MSELoss()
 
-    def create_feature_extractor(self) -> nn.Module:
-        features_dim = (self.output_dim + self.embedding_dim) // 2
-        return nn.Sequential(
-            nn.Linear(self.embedding_dim, features_dim, bias=True),
-            nn.Softplus(),
-        )
-
     def forward(self, embedding: torch.Tensor) -> torch.Tensor:
-        features = self.features(embedding)
-        action_quality = self.q_net(features)
+        action_quality = self.action_quality_net(embedding)
 
         if self.dueling_dqn:
-            value = self.dueling_q_net(features)
-            action_quality = action_quality + value - torch.mean(action_quality, dim=-1, keepdim=True)
+            value = self.value_net(embedding)
+            action_quality_mean = torch.mean(action_quality, dim=-1, keepdim=True)
+            action_quality = action_quality - action_quality_mean
+            action_quality = action_quality + value
 
         return action_quality
 
@@ -61,7 +54,8 @@ class QualityNetwork(nn.Module):
                action: torch.Tensor,
                reward: Union[float, torch.Tensor],
                next_embedding,
-               next_quality=None):
+               next_quality=None,
+               done: bool = False):
 
         action = action.long()
         quality = self.forward(embedding)
@@ -75,7 +69,7 @@ class QualityNetwork(nn.Module):
             # next_quality = torch.gather(next_quality, 1, action)
 
         # temporal difference residual
-        true_quality = reward + self.discount_factor * next_quality
+        true_quality = reward + (1 - done) * self.discount_factor * next_quality
         advantage = true_quality - quality
 
         loss = self.criterion(true_quality, quality)
@@ -83,6 +77,8 @@ class QualityNetwork(nn.Module):
         return loss, advantage
 
     def fit(self, embedding: torch.Tensor, true_quality: torch.Tensor):
+        warnings.warn("Method QualityHead.fit is deprecated", DeprecationWarning)
+
         quality = self.forward(embedding)
         loss = self.criterion(quality, true_quality)
 
